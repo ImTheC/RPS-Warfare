492a861efc14b9c56632baa37ff18945a8b68520
Christian Castro <castroMBair@MacBook-Air.local>
2016-08-24 11:27:45 -0500

    Fix User Management Bug

--- webapp/ng/controllers/users.ctr.js
+++ webapp/ng/controllers/users.ctr.js
@@ -28,76 +28,56 @@
         s.message = null;
         s.error = null;
         authService.$createUserWithEmailAndPassword(s.email,s.password)
-          .then(function(firebaseUser) {
 
-            s.firebaseUser = firebaseUser;
-
-            Object.defineProperties(s.firebaseUser.providerData[0],{
-              displayName: { writable:true },
-              email: { writable:true },
-              photoURL: { writable:true }
-            });
-
-            s.firebaseUser.providerData[0].displayName;
-            $scope.$watch('firebaseUser.providerData[0].displayName', function(){
-                if (s.firebaseUser){
-                  s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
-                }
-              }
-            );
-            s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
-
-            s.firebaseUser.providerData[0].photoURL = 'assets/images/no-photo.gif';
-
-            s.firebaseUser.providerData[0].score = 0;
-
-            s.firebaseUser.providerData[0].dateCreated = firebase.database.ServerValue.TIMESTAMP;
-
-            s.firebaseUser.providerData[0].inGames = [''];
-
-            if (s.firebaseUser !== null && s.firebaseUser.providerData[0].providerId === 'password'){
-
-              let uid = s.firebaseUser.uid;
-
-              let providerData = s.firebaseUser.providerData[0];
-
-              addUserService.toUserObj(uid).$add(providerData)
-              .then(function(ref) {
-                // let id = ref.key();
-                console.log('Email Signup Saved');
-              }, function(error) {
-                console.log("Oops, the following went wrong: ", error);
-              });
-
-            } else {
-              console.log('is logged out');
-            }
-
-
+        .then(function(firebaseUser) {
+          let userURL = `https://rps-warfare.firebaseio.com/usermap/${firebaseUser.uid}.json`;
+          $http({
+            method: 'GET',
+            url: userURL
           })
-          .catch(function(error) {
-            s.error = error;
+          .then(function(data){
+            if (data.data === null){
+                  console.log('Safe to add new user.');
+                  let newUserObj = firebaseUser.providerData[0];
+
+                  Object.defineProperties(newUserObj,{
+                    displayName: { writable:true },
+                    email: { writable:true },
+                    photoURL: { writable:true }
+                  });
+                  newUserObj.displayName = newUserObj.email.replace(/@.*/, '');
+                  newUserObj.photoURL = 'assets/images/no-photo.gif';
+
+                  newUserObj.dateCreated = firebase.database.ServerValue.TIMESTAMP;
+                  newUserObj.score = 0;
+                  newUserObj.inGames = [''];
+                  newUserObj.aid = firebaseUser.uid;
+                  newUserObj.emailVerified = firebaseUser.emailVerified;
+                  s.aid = newUserObj.aid;
+                  // newUserObj.token = firebaseUser.refreshToken;
+
+                  addUserService.addToDb('users').$add(newUserObj)
+                  .then(function(ref) {
+                    s.newUserObjKey = ref.key;
+                    let value = ref.key;
+                    let key = newUserObj.aid;
+                    let newUserMapObj = {};
+                    newUserMapObj[key] = value;
+                    firebase.database().ref('usermap/').update(newUserMapObj);
+                  }, function(error) {
+                    console.log("Oops, new user was not added. Error: ",error);
+                  });
+            } else {
+              console.log('Account already exists.');
+              return;
+            }
           });
+        })
+        .catch(function(error) {
+          console.error("Failed to login. Error: ",error);
+        });
+
       };
-
-
-      // function facebookSignin(){
-      //
-      //   let provider = new firebase.auth.FacebookAuthProvider();
-      //
-      //   firebase.auth().signInWithPopup(provider)
-      //
-      //     .then(function(result) {
-      //       let token = result.credential.accessToken;
-      //       console.log(token);
-      //       s.firebaseUser = result.user;
-      //       console.log(s.firebaseUser);
-      //     })
-      //     .catch(function(error) {
-      //       console.log(error);
-      //     });
-      //
-      // }
 
       //SOCIAL SIGNUP
       function facebookSignin(){
@@ -109,52 +89,36 @@
             url: userURL
           })
           .then(function(data){
-            console.log('http get data request: ',data.data);
             if (data.data === null){
-                  console.log('data was null, therefore saving new record to DB');
+                  console.log('Safe to add new user.');
                   let newUserObj = result.user.providerData[0];
                   newUserObj.dateCreated = firebase.database.ServerValue.TIMESTAMP;
                   newUserObj.score = 0;
                   newUserObj.inGames = [''];
                   newUserObj.aid = result.user.uid;
+                  newUserObj.emailVerified = result.user.emailVerified;
                   s.aid = newUserObj.aid;
-                  newUserObj.token = result.credential.accessToken;
+                  // newUserObj.token = result.credential.accessToken;
 
                   addUserService.addToDb('users').$add(newUserObj)
                   .then(function(ref) {
                     s.newUserObjKey = ref.key;
-                    console.log('ref after user object added to /users ',ref);
-                    console.log('newUserObjKey after user object added to /users ',s.newUserObjKey);
-                    console.log('this was saved to firebaseUser and to scope: ',newUserObj);
                     let value = ref.key;
                     let key = newUserObj.aid;
-                    addUserMapService.addToDb(key).$add(value)
-                      .then(function(ref){
-                        let usermapKey = ref.key;
-                        let pathURL = `https://rps-warfare.firebaseio.com/users/${s.newUserObjKey}.json`;
-                        $http({
-                          method: 'PATCH',
-                          url: pathURL,
-                          data: {
-                            usermapKey: usermapKey
-                          }
-                        })
-                          .then(function(){console.log('made it back');},function(error){console.log(error.code,error.message);});
-                        console.log('record added to usermap');
-                      },function(error){
-                        console.log(error);
-                      });
+                    let newUserMapObj = {};
+                    newUserMapObj[key] = value;
+                    firebase.database().ref('usermap/').update(newUserMapObj);
                   }, function(error) {
-                    console.log("Oops, the following went wrong: ", error);
+                    console.log("Oops, new user was not added. Error: ",error);
                   });
             } else {
-              console.log('data exists already, therefore no new record being created');
+              console.log('Account already exists.');
               return;
             }
           });
         })
         .catch(function(error) {
-          console.error("Authentication failed: ", error);
+          console.error("Failed to login. Error: ",error);
         });
       }
 
@@ -174,22 +138,24 @@
       function signInWithEmail(email,password){
         authService.$signInWithEmailAndPassword(email,password)
         .then(function(firebaseUser) {
+          let userURL = `https://rps-warfare.firebaseio.com/usermap/${firebaseUser.uid}.json`;
+          $http({
+            method: 'GET',
+            url: userURL
+          })
+            .then(function(data){
+              let ref = data.data;
+              let clonedUsersURL = `https://rps-warfare.firebaseio.com/users/${ref}.json`;
+                $http({
+                  method: 'GET',
+                  url: clonedUsersURL
+                })
+                  .then(function(data){
+                    s.firebaseUser = data.data;
+                    console.log(s.firebaseUser);
+                  },function(error){ console.log(error); })
+            },function(error){ console.log('error message: ', error)});
 
-          s.firebaseUser = firebaseUser;
-
-          Object.defineProperties(s.firebaseUser.providerData[0],{
-            displayName: { writable:true },
-            email: { writable:true }
-          });
-
-          s.firebaseUser.providerData[0].displayName;
-          $scope.$watch('firebaseUser.providerData[0].displayName', function(){
-              if (s.firebaseUser){
-                s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
-              }
-            }
-          );
-          s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
         })
         .catch(function(error) {
             console.error("Oops, looks like we couldn't log you in for the following reason: ", error);
